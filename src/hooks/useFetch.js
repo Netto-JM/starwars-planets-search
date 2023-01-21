b1fbd05a59fdc33f2c077d2eefeb5e167766d726
NettoJM <nettojm86@gmail.com>
2023-01-20 21:54:54 -0300

    implementing "useFetch" custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,33 +1,56 @@
-import { useState } from 'react';
+// inspiration for this hook is a combination from live lectures bellow:
+// https://www.smashingmagazine.com/2020/07/custom-react-hook-fetch-cache-data/
+// and the article bellow:
+// https://www.smashingmagazine.com/2020/07/custom-react-hook-fetch-cache-data/
 
-function useFetch() {
+import { useState, useEffect, useRef } from 'react';
+
+function useFetch(url) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+  const [data, setData] = useState();
+  const cache = useRef({});
 
-  const makeFetch = async (url) => {
-    try {
-      setIsLoading(true);
+  useEffect(() => {
+    if (!url) return;
 
-      const response = await fetch(url);
-      const apiResponse = await response.json();
-
-      if (!response.ok) {
+    const handleError = ({ ok, status }, apiResponse) => {
+      if (!ok) {
         const apiError = new Error(
-          `The endpoint ${url} responded with status code: ${response.status}`,
+          `The endpoint ${url} responded with status code: ${status}`,
         );
         apiError.response = apiResponse;
         throw apiError;
       }
+    };
 
+    const handleResponse = async (response) => {
+      const apiResponse = await response.json();
+      handleError(response, apiResponse);
       return apiResponse;
-    } catch (e) {
-      setError(e);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    };
 
-  return { isLoading, error, makeFetch };
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        if (cache.current[url]) {
+          const cachedResponse = cache.current[url];
+          return handleResponse(cachedResponse);
+        }
+        const response = await fetch(url);
+        cache.current[url] = response;
+        return handleResponse(response);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData()
+      .then(setData);
+  }, [url]);
+
+  return { isLoading, error, data };
 }
 
 export default useFetch;
