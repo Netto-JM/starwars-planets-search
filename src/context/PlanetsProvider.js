e780b09ec20eff7fc3d717e321add7ab09d01a56
NettoJM <nettojm86@gmail.com>
2023-01-22 14:06:38 -0300

    refactored PlanetsProvider to make it work properly

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 import useFetch from '../hooks/useFetch';
 
 export default function PlanetsProvider({ children }) {
-  const [planets, setPlanets] = useState([]);
+  const getPlanets = ({ results }) => results.map(({ residents, ...rest }) => (rest));
 
-  const { isLoading, error, data: { results } } = useFetch('https://swapi.dev/api/planets');
+  const fetchedData = useFetch('https://swapi.dev/api/planets', getPlanets);
 
-  const filtRes = results.map(({ residents, ...planetRest }) => (planetRest));
-
-  setPlanets(filtRes);
+  const { isLoading, error, data } = useMemo(() => ({
+    isLoading: fetchedData.isLoading,
+    error: fetchedData.error,
+    data: fetchedData.data,
+  }), [fetchedData]);
 
   return (
-    <PlanetsContext.Provider value={ { isLoading, error, planets } }>
+    <PlanetsContext.Provider value={ { isLoading, error, data } }>
       {children}
     </PlanetsContext.Provider>
   );
