408061067d81cc7d315d13a722d55b54efb54645
NettoJM <nettojm86@gmail.com>
2023-01-22 15:35:29 -0300

    created filter and provider to provide a way to filter planets

---
+++ src/context/FilterProvider.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import FilterContext from './FilterContext';
+import PlanetsContext from './PlanetsContext';
+
+export default function FilterProvider({ children }) {
+  const [nameFilter, setNameFilter] = useState('');
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const { planets } = useContext(PlanetsContext);
+
+  const byName = (SWPlanets, letters) => (
+    SWPlanets.filter(({ name }) => name.includes(letters))
+  );
+
+  useEffect(() => {
+    // if (planets) {
+    //   if (nameFilter) setFilteredPlanets(byName(planets, nameFilter));
+    // }
+    if (planets && nameFilter) setFilteredPlanets(byName(planets, nameFilter));
+  }, [nameFilter]);
+
+  return (
+    <FilterContext.Provider value={ { setNameFilter, filteredPlanets } }>
+      {children}
+    </FilterContext.Provider>
+  );
+}
+
+FilterProvider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.elementType),
+}.isRequired;
