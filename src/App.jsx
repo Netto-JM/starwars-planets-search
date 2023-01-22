0a226a24083a2e74d2cf6ed8815fc001244195d4
NettoJM <nettojm86@gmail.com>
2023-01-22 14:07:34 -0300

    rendering Table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import useFetch from './hooks/useFetch';
+import Table from './components/Table';
 
 function App() {
-  const { isLoading, error, data } = useFetch('https://swapi.dev/api/planets');
-  console.log(isLoading, error, data);
   return (
-    <span>Hello, App!</span>
+    <Table />
   );
 }
 
