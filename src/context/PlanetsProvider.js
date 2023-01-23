1f10eedadba5a4fefb378a1b85dda8a15469397b
NettoJM <nettojm86@gmail.com>
2023-01-22 23:42:39 -0300

    refactored to get headings from the fetchedData

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -4,18 +4,23 @@ import PlanetsContext from './PlanetsContext';
 import useFetch from '../hooks/useFetch';
 
 export default function PlanetsProvider({ children }) {
-  const getPlanets = ({ results }) => results.map(({ residents, ...rest }) => (rest));
+  const getPlanets = ({ results }) => {
+    const planets = results.map(({ residents, ...rest }) => (rest));
+    const headings = Object.keys(planets[0]).map((planet) => planet.replace('_', ' '));
+    return { planets, headings };
+  };
 
-  const fetchedData = useFetch('https://swapi.dev/api/planets', getPlanets);
+  const fetchedData = useFetch('https://swapi.dev/api/planets', getPlanets, {});
 
-  const { isLoading, error, planets } = useMemo(() => ({
+  const { isLoading, error, planets, headings } = useMemo(() => ({
     isLoading: fetchedData.isLoading,
     error: fetchedData.error,
-    planets: fetchedData.data,
+    planets: fetchedData.data.planets,
+    headings: fetchedData.data.headings,
   }), [fetchedData]);
 
   return (
-    <PlanetsContext.Provider value={ { isLoading, error, planets } }>
+    <PlanetsContext.Provider value={ { isLoading, error, planets, headings } }>
       {children}
     </PlanetsContext.Provider>
   );
