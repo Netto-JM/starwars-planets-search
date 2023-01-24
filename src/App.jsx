f0df5ad180ab4d9b9551f927e8bcf3552adf0c45
NettoJM <nettojm86@gmail.com>
2023-01-24 17:40:42 -0300

    using PlanetsContext data to implement loadinh and error handling in the future

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from './context/PlanetsContext';
 import './App.css';
+import Filter from './components/Filters';
 import Table from './components/Table';
 
 function App() {
+  const { isLoading, error } = useContext(PlanetsContext);
+  console.log('render table conditionally:', isLoading, error);
   return (
-    <Table />
+    <>
+      <Filter />
+      <Table />
+    </>
   );
 }
 
