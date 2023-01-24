9a117e6d7844ba377028ca48cf9f4359d5e7f0cc
NettoJM <nettojm86@gmail.com>
2023-01-24 17:39:24 -0300

    removed Filter based code to a separate component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,18 +3,8 @@ import PlanetsContext from '../context/PlanetsContext';
 import FilterContext from '../context/FilterContext';
 
 function Table() {
-  const { isLoading, error, headings } = useContext(PlanetsContext);
-  const { setters, values, handlers } = useContext(FilterContext);
-  const { filteredPlanets, columnOptions, columnValue } = values;
-  const { handleFilter, handleClear } = handlers;
-  const {
-    setNameFilter,
-    setColumnValue,
-    setComparisonValue,
-    setNumberValue,
-  } = setters;
-
-  console.log('to be used:', isLoading, error);
+  const { headings } = useContext(PlanetsContext);
+  const { values: { filteredPlanets } } = useContext(FilterContext);
 
   const tableHeadings = headings.map((heading) => (
     <th key={ heading }>{heading}</th>
@@ -26,51 +16,8 @@ function Table() {
     return <tr key={ JSON.stringify(data) }>{planetData}</tr>;
   });
 
-  const columnFilterOptions = columnOptions.map((option) => (
-    <option key={ option } value={ option }>{option}</option>
-  ));
-
   return (
     <div>
-      <input
-        type="text"
-        data-testid="name-filter"
-        onChange={ ({ target }) => { setNameFilter(target.value); } }
-      />
-      <label htmlFor="column" data-testid="column-filter">
-        Coluna:
-        <select
-          name="column"
-          id="column"
-          value={ columnValue }
-          onChange={ ({ target }) => { setColumnValue(target.value); } }
-        >
-          {columnFilterOptions}
-        </select>
-      </label>
-      <label htmlFor="comparison" data-testid="comparison-filter">
-        Operador:
-        <select
-          name="comparison"
-          id="column"
-          onChange={ ({ target }) => { setComparisonValue(target.value); } }
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <input
-        type="number"
-        data-testid="value-filter"
-        onChange={ ({ target }) => { setNumberValue(target.value); } }
-      />
-      <button type="button" data-testid="button-filter" onClick={ handleFilter }>
-        FILTRAR
-      </button>
-      <button type="button" data-testid="button-remove-filters" onClick={ handleClear }>
-        REMOVER FILTROS
-      </button>
       <table>
         <thead>
           <tr>{tableHeadings}</tr>
