1eed51aed23fc0bbc14bb91441eafbd6887f473a
NettoJM <nettojm86@gmail.com>
2023-01-24 22:38:38 -0300

    using helper "renderWithContext" in tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from '../App';
+import { renderWithContext } from './helpers/renderWithContext';
 
 test('I am your test', () => {
-  render(<App />);
+  renderWithContext(<App />);
   const linkElement = screen.getByText(/Hello, App!/i);
   expect(linkElement).toBeInTheDocument();
 });
