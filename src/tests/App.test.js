ed682861e43a6e4f8c2aeff5c61a16b54a366aac
NettoJM <nettojm86@gmail.com>
2023-01-25 03:24:54 -0300

    initial Table component tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import App from '../App';
+import mockFetch from '../__mocks__/mockFetch';
 import { renderWithContext } from './helpers/renderWithContext';
 
-test('I am your test', () => {
-  renderWithContext(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Testes dos componentes <Table.jsx /> e <Filter.jsx />', () => {
+  beforeEach(() => {
+    global.fetch = mockFetch;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('testa se os dados iniciais do componente <Table.jsx /> são carregados corretamente', async () => {
+    renderWithContext(<App />);
+    const table = await screen.findByRole('table');
+    expect(table).toBeInTheDocument();
+    const tableHeaders = await screen.findAllByRole('columnheader');
+    expect(tableHeaders).toHaveLength(13)
+  });
 });
