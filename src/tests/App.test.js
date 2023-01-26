36aad95b8849cc6dd0e316e5463c997035a1a3dd
NettoJM <nettojm86@gmail.com>
2023-01-26 17:08:31 -0300

    testing filter by name

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import mockFetch from '../__mocks__/mockFetch';
 import { renderWithContext } from './helpers/renderWithContext';
@@ -29,6 +30,31 @@ describe('Testes dos componentes <Table.jsx /> e <Filter.jsx />', () => {
     expect(tableHeaders).toHaveLength(13);
     const tableRows = screen.getAllByRole('row');
     expect(tableRows).toHaveLength(11);
-
   });
+
+  it('testa se o filtro de nome funciona e é case-sensitive', async () => {
+    await loadInitialData();
+    const nameInput = screen.getByTestId('name-filter');
+    
+    userEvent.type(nameInput, 'oO');
+    expect(screen.getAllByRole('row')).toHaveLength(3);
+    const tatooineName = screen.getByRole('cell', {
+      name: /tatooine/i,
+    })
+    const nabooName = screen.getByRole('cell', {
+      name: /naboo/i,
+    })
+    expect(tatooineName).toBeInTheDocument();
+    expect(nabooName).toBeInTheDocument();
+
+    userEvent.clear(nameInput);
+    userEvent.type(nameInput, 'bE');
+    expect(screen.getAllByRole('row')).toHaveLength(2);
+    const bespinName = screen.getByRole('cell', {
+      name: /bespin/i
+    })
+    expect(bespinName).toBeInTheDocument();
+    expect(tatooineName).not.toBeInTheDocument();
+    expect(nabooName).not.toBeInTheDocument();
+});
 });
