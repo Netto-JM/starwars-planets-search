03cde55c9f6312e29a8162cae6197bc0fa0636ed
NettoJM <nettojm86@gmail.com>
2023-02-01 19:56:08 -0300

    separated fetch mocks into mockFetchResolved and mockFetchRejected

--- src/__mocks__/mockFetch.js
+++ src/__mocks__/mockFetch.js
@@ -1,9 +1,13 @@
 import testData from '../../cypress/mocks/testData';
 
-const mockFetch = () => Promise.resolve({
+export const mockFetchResolved = () => Promise.resolve({
   status: 200,
   ok: true,
   json: () => Promise.resolve(testData),
 });
 
-export default mockFetch;
+export const mockFetchRejected = () => Promise.resolve({
+  status: 404,
+  ok: false,
+  json: () => Promise.resolve('Something wrong happened'),
+});
